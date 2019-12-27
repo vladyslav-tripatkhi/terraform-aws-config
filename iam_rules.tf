31317245d92c908dce9db3aba9b0c571f54e1e79
vladyslav-tripatkhi <vladyslavtr@wix.com>
2019-12-27 13:54:12 +0200

    Added a couple of IAM rules

--- iam_rules.tf
+++ iam_rules.tf
@@ -1,16 +1,3 @@
-resource "aws_iam_account_password_policy" "this" {
-    count = var.password_policy_create ? 1 : 0
-    
-    minimum_password_length = var.password_minimum_length
-    max_password_age = var.password_max_age
-    password_reuse_prevention = var.password_reuse_prevention
-    require_numbers = var.password_require_numbers
-    require_symbols = var.password_require_symbols
-    require_lowercase_characters = var.password_require_lowercase
-    require_uppercase_characters = var.password_require_uppercase
-    allow_users_to_change_password = var.allow_users_to_change_password
-}
-
 data "template_file" "iam_password_policy" {
     template = file("${path.module}/templates/iam_password_policy.tpl")
     vars = {
@@ -46,5 +33,43 @@ resource "aws_config_config_rule" "iam_user_mfa_enabled_rule" {
         source_identifier = "IAM_USER_MFA_ENABLED"
     }
 
+    depends_on = [aws_config_delivery_channel.this]
+}
+
+resource "aws_config_config_rule" "iam-user-unused-credentials-check" {
+    name = "iam-user-unused-credentials-check"
+    description = ""
+
+    source {
+        owner = "AWS"
+        source_identifier = "IAM_USER_UNUSED_CREDENTIALS_CHECK"
+    }
+
+    input_parameters = "{\"maxCredentialUsageAge\":\"90\"}"
+    depends_on = [aws_config_delivery_channel.this]
+}
+
+resource "aws_config_config_rule" "access-keys-rotated" {
+    name = "access-keys-rotated"
+    description = ""
+
+    source {
+        owner = "AWS"
+        source_identifier = "ACCESS_KEYS_ROTATED"
+    }
+
+    input_parameters = "{\"maxAccessKeyAge\":\"90\"}"
+    depends_on = [aws_config_delivery_channel.this]
+}
+
+resource "aws_config_config_rule" "mfa-enabled-for-iam-console-access" {
+  name = "mfa-enabled-for-iam-console-access"
+    description = ""
+
+    source {
+        owner = "AWS"
+        source_identifier = "MFA_ENABLED_FOR_IAM_CONSOLE_ACCESS"
+    }
+    
     depends_on = [aws_config_delivery_channel.this]
 }
